80654ef3df786bd6863718ba59290f2ec1aa236f
maksoy <mertaksoy@Merts-MacBook-Air.local>
2022-12-22 15:44:29 +0300

    Jump fixed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -42,33 +42,13 @@ public class Movement : MonoBehaviour
 	private void MovePlayer()
 	{
 		moveDirection = (transform.forward * verticalInput + transform.right * horizontalInput).normalized;
-		moveDirection.y = rb.velocity.y;
-		rb.velocity = (moveDirection * speed);
+		var velo = (moveDirection * speed);
+		rb.velocity = new Vector3(velo.x, rb.velocity.y, velo.z);
 	}
 
 	// Update is called once per frame
-	/*
 	void Update()
     {
-		if (Input.GetKey(KeyCode.D))
-		{
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.A))
-		{
-			transform.position += Vector3.left * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.W))
-		{
-			transform.position += Vector3.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			transform.position += Vector3.back * speed * Time.deltaTime;
-		}
-        if (Input.GetKey(KeyCode.Space))
-        {
-			transform.position += Vector3.up * speed * Time.deltaTime;
-        }
-	}*/
+		
+	}
 }
