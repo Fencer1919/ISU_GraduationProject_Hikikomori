1ebec570c42d26b18c593a18f56f993b00acd1fd
zey <zeynepkalaycioglu@windowslive.com>
2023-03-23 16:34:11 +0300

    BossScene: Countdown Added & Models implemented

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private Image time;
+    [SerializeField] private Text timeText;
+    private float currentTime;
+    [SerializeField] private float duration;
+    void Start()
+    {
+        currentTime = duration;
+        timeText.text = currentTime.ToString();
+        StartCoroutine(CountdownTime());
+    }
+
+    private IEnumerator CountdownTime () {
+        while(currentTime >= 0) {
+            time.fillAmount = Mathf.InverseLerp(0, duration, currentTime);
+            timeText.text = currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+        yield return null;
+    }
+}
