97e4bb2ceb618f12b8c8bc6057dd22150533c7c1
Fencer2021 <mert@ekipmobilya.com>
2023-04-23 03:17:36 +0300

    More Letters Placed, Scroll Work On Progress
    More Letters Placed, Scroll Work On Progress

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,22 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
-{ 
-   public GameObject letterPanel;
+{
+    public GameObject letterPanel;
+    public ScrollRect letterScrollRect;
 
+    public void CloseLetter()
+    {
+        letterPanel.SetActive(false);
+    }
 
-   public void CloseLetter()
-   {
-      letterPanel.SetActive(false);
-   }
-   
-   public void OpenLetter()
-   {
-      letterPanel.SetActive(true);
-   }
-   
-
-   
+    public void OpenLetter()
+    {
+        letterPanel.SetActive(true);
+        letterScrollRect.verticalNormalizedPosition = 1f; // scroll to top
+    }
 }
