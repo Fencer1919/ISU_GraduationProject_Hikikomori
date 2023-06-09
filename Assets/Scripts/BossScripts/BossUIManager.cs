12f5c4f2de8065eba86a1e32e4a42951be3dfe61
zey <zeynepkalaycioglu@windowslive.com>
2023-06-09 18:28:57 +0300

    last fixes

--- Assets/Scripts/BossScripts/BossUIManager.cs
+++ Assets/Scripts/BossScripts/BossUIManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossUIManager : MonoBehaviour
 { 
     public GameObject bossCompletedPanel;
     public GameObject timerPanel;
+    public string nextSceneName;
     public Countdown countdown;
+    public string bossSceneCheck = "-";
 
     public PlayerBossMovement playerBossMovement;
 
@@ -20,10 +23,22 @@ public class BossUIManager : MonoBehaviour
     }
     public void OnBossSuccess()
     {
+    if(bossSceneCheck == "boss")
+    {
+        playerBossMovement.StopMovement();
+        timerPanel.SetActive(false);
+        SoundManager.Instance.sceneCheck = "normal";
+        SoundManager.Instance.OnSceneChanged();
+        SceneManager.LoadScene(nextSceneName);
+    }
+    else
+    {
         playerBossMovement.StopMovement();
         bossCompletedPanel.SetActive(true);
         timerPanel.SetActive(false);
         SoundManager.Instance.sceneCheck = "normal";
         SoundManager.Instance.OnSceneChanged();
+    }
+       
     }
 }
